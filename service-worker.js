eee9154467bd3e987a20560f144d21e6925fde9e
danielrogers0245-blip <danielrogers0245@gmail.com>
2026-03-09 21:45:23 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'bjj-tracker-v8-cronometer-aligned';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
